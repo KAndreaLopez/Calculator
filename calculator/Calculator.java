2be10d904292dc758da8effbdba6949e2fbf252e
KAndreaLopez <53548758+KAndreaLopez@users.noreply.github.com>
2019-09-14 00:42:52 -0600

    Add files via upload

---
+++ calculator/Calculator.java
@@ -0,0 +1,19 @@
+
+package calculator;
+
+/**
+ *
+ * @author Katherine Andrea López Muñoz
+ * 5090-18-81
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana calculadora= new Ventana();
+        calculadora.setVisible(true);
+    }
+    
+}
